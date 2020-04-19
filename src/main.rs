901967d9bb236131a83f5b5d0ce956f4d66ae6e3
Niharika <niharika.3297@gmail.com>
2020-04-19 22:34:33 +0530

    Add templating engine

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+#![feature(plugin)]
+// #![plugin(rocket_codegen)]
+
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
 
+use std::collections::HashMap;
+use rocket_contrib::templates::Template;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context = HashMap::<String, String>::new();
+    Template::render("index", context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
