d09b7ded216023d5707da11f49ad1a61c03ff2ef
Niharika <niharika.3297@gmail.com>
2020-04-28 21:43:50 +0530

    Refine templating

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
 #![feature(plugin)]
-// #![plugin(rocket_codegen)]
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
 
-use std::collections::HashMap;
-use rocket_contrib::templates::Template;
+use rocket::Request;
+use rocket::response::Redirect;
+use rocket_contrib::templates::{Template, handlebars};
+use rocket_contrib::serve::StaticFiles;
+
+use handlebars::{Helper, Handlebars, Context, RenderContext, Output, HelperResult, JsonRender};
+
+#[derive(Serialize)]
+struct TemplateContext {
+    title: &'static str,
+    items: Vec<&'static str>,
+    // This key tells handlebars which template is the parent.
+    parent: &'static str,
+}
 
 #[get("/")]
 fn index() -> Template {
-    let context = HashMap::<String, String>::new();
-    Template::render("index", context)
+    Template::render("index", &TemplateContext {
+        title: "Reflect",
+        items: vec!["Home 🏡", "New Entry➕", "View Entry👀"],
+        parent: "layout",
+    })
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> Template {
+    let mut map = std::collections::HashMap::new();
+    map.insert("path", req.uri().path());
+    Template::render("error/404", &map)
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", routes![index])
+        .mount("/", StaticFiles::from("static/"))
+        .register(catchers![not_found])
         .attach(Template::fairing())
         .launch();
 }
